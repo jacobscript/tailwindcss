6b68a04d26ce5522e596f621e25df97edbb7f4cc
jacobscript <56893429+jacobscript@users.noreply.github.com>
2021-06-13 19:24:28 -0400

    Add files via upload

---
+++ tailwind-default.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontSize: {
+                mammoth: '8rem',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
